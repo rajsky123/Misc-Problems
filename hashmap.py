03b9e936b556254d75c5b5cabd95ee0a7314b70c
rajsky123 <34422954+rajsky123@users.noreply.github.com>
2017-12-12 01:10:49 +0530

    Delete hashmap.py

--- hashmap.py
+++
@@ -1,9 +0,0 @@
-a={}
-nn=input("enter range:")
-for i in range(1,int(nn)+1):
-    strr=input("enter "+str(i)+":")
-    if(a.get(strr)!=None):
-        a[strr]=a[strr]+1
-    else:
-        a[strr]=1
-print(a)
